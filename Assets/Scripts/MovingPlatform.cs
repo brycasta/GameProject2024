6fa819a16b21cc1db8fee2f853a15902b92742e0
brycasta <BRYANGRINGO@HOTMAIL.COM>
2024-11-05 23:42:56 -0600

    My updates
    Added moving platform, wall barrier, falling asteroids, added restart and main mneu button functionality

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    private Vector3 lastPosition;
+    private GameObject playerOnPlatform;
+
+    void Start()
+    {
+        lastPosition = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        // Calculate the platform's velocity
+        Vector3 platformVelocity = (transform.position - lastPosition) / Time.fixedDeltaTime;
+        lastPosition = transform.position;
+
+        // Apply the platform's velocity to the player if they are on the platform
+        if (playerOnPlatform != null)
+        {
+            Rigidbody2D playerRb = playerOnPlatform.GetComponent<Rigidbody2D>();
+            if (playerRb != null)
+            {
+                playerRb.velocity += new Vector2(platformVelocity.x, 0);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerOnPlatform = collision.gameObject;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerOnPlatform = null;
+        }
+    }
+}
